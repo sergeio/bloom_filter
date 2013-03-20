124c8fa044717ee66d718d913aa37bf9e83f0281
Sergei Orlov <sergeio@aweber.com>
2013-03-19 18:02:06 -0700

    use 3 lists

--- bloom_filter.py
+++ bloom_filter.py
@@ -4,24 +4,27 @@ class BloomSet(object):
     def __init__(self, elements):
         self.len1 = max(len(elements) / 2, 10)
         self.len2 = max(len(elements) / 2 + 1, 11)
+        self.len3 = max(len(elements) / 2 + 2, 12)
         self.arr1 = [0] * self.len1
         self.arr2 = [0] * self.len2
+        self.arr3 = [0] * self.len3
 
         for element in elements:
             self.add_element(element)
 
     def add_element(self, element):
-        index1, index2 = self._hashes(element)
+        index1, index2, index3 = self._hashes(element)
         self.arr1[index1] = 1
         self.arr2[index2] = 1
+        self.arr3[index3] = 1
 
     def _hashes(self, element):
         h = _hash(element)
-        return h % self.len1, h % self.len2
+        return h % self.len1, h % self.len2, h% self.len3
 
     def contains(self, element):
-        index1, index2 = self._hashes(element)
-        return self.arr1[index1] and self.arr2[index2]
+        index1, index2, index3 = self._hashes(element)
+        return self.arr1[index1] and self.arr2[index2] and self.arr3[index3]
 
 
 def _hash(num):
--- test_practical.py
+++ test_practical.py
@@ -1,11 +1,28 @@
 import random
+import time
+
+from bloom_filter import BloomSet
 
 
 def main():
-    max_num = 10000000
-    numbers = [random.randint(1, max_num) for i in xrange(max_num)]
-    print len(numbers)
+    print 'creating'
+    numbers = range(100000000)
+    print 'created'
 
+    # rset = set(numbers)
+    bset = BloomSet(numbers)
+    print 'set done'
+
+    # test_membership_rset(numbers, rset)
+    test_membership_bset(numbers, bset)
+
+def test_membership_rset(_list, rset):
+    for element in _list:
+        assert element in rset
+
+def test_membership_bset(_list, bset):
+    for element in _list:
+        assert bset.contains(element)
 
 
 if __name__ == '__main__':
