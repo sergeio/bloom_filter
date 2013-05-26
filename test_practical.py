d3a96fc6ec94d8418a5c90ad8f055628dc9dc62f
Sergei Orlov <sergeio@aweber.com>
2013-05-25 21:16:41 -0400

    Add benchmarking tests

--- test_practical.py
+++ test_practical.py
@@ -1,7 +1,10 @@
 import random
 import time
+# from guppy import hpy
 
 from bloom_filter import BloomSet
+
+# h = hpy()
 
 
 def main():
@@ -24,6 +27,78 @@ def test_membership_bset(_list, bset):
     for element in _list:
         assert bset.contains(element)
 
+def test_bset(num_entries):
+    # start_size = h.heap().size
+
+    start = time.time()
+    bset = BloomSet(xrange(num_entries))
+    created_time = time.time()
+
+    # created_size = h.heap().size
+
+    start_positives_time = time.time()
+    for i in xrange(num_entries):
+        assert bset.contains(i)
+    checked_positives_time = time.time()
+
+    for i in xrange(num_entries, num_entries * 2):
+        bset.contains(i)
+    checked_negatives_time = time.time()
+
+    t_create = created_time - start
+    t_pos = checked_positives_time - start_positives_time
+    t_neg = checked_negatives_time - checked_positives_time
+    # k_size = int((created_size - start_size) / 1000.0)
+
+    print '''
+    num_entries: {0}
+    create time: {1}s
+    check positives time: {2}s
+    check negatives time: {3}s
+    '''.format(num_entries, t_create, t_pos, t_neg)
+
+
+def test_rset(num_entries):
+    # start_size = h.heap().size
+
+    start = time.time()
+    rset = set(xrange(num_entries))
+    created_time = time.time()
+
+    # created_size = h.heap().size
+
+    start_positives_time = time.time()
+    for i in xrange(num_entries):
+        assert i in rset
+    checked_positives_time = time.time()
+
+    for i in xrange(num_entries, num_entries * 2):
+        i in rset
+    checked_negatives_time = time.time()
+
+    t_create = created_time - start
+    t_pos = checked_positives_time - start_positives_time
+    t_neg = checked_negatives_time - checked_positives_time
+    # k_size = int((created_size - start_size) / 1000.0)
+
+    print '''
+    num_entries: {0}
+    create time: {1}s
+    check positives time: {2}s
+    check negatives time: {3}s
+    '''.format(num_entries, t_create, t_pos, t_neg)
+
+
+
 
 if __name__ == '__main__':
-    main()
+    print 'bset'
+    test_bset(int(1e6))
+    test_bset(int(1e5))
+    test_bset(int(1e4))
+
+    print 'rset'
+    test_rset(int(1e6))
+    test_rset(int(1e5))
+    test_rset(int(1e4))
+    # main()
